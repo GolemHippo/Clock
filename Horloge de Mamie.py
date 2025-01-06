13b84b5c0089be9e712c8966af9ecacd1f549ced
axel-achart <axel.achart@gmail.com>
2025-01-06 12:29:46 +0100

    12h and 24h format choose

--- Horloge de Mamie.py
+++ Horloge de Mamie.py
@@ -1,6 +1,8 @@
 # Importing the library
 import time
 
+
+
 # Function that displays local time
 def localtime():
     print()
@@ -11,6 +13,16 @@ def localtime():
         seconds = time.strftime('%S')
         print(hours,':',minutes,':',seconds)
         time.sleep(1)
+# Version 12h
+def localtime2():
+    print()
+    while True:
+        global hours, minutes, seconds
+        hours = time.strftime('%r')
+        print(hours)
+        time.sleep(1)
+
+
 
 # Function that set an alarm
 def alarm():
@@ -29,14 +41,57 @@ def alarm():
         time.sleep(1)
         if hours_alarm == hours and minutes_alarm == minutes and seconds_alarm == seconds:
             print("ALARM !!!")
+# Version 12h
+def alarm2():
+    print()
+    print("Set an alarm...")
+    hours_alarm = input("Enter the hour : ")
+    minutes_alarm = input("Enter the minute : ")
+    seconds_alarm = input("Enter the second : ")
+    print(f"Alarm set at {hours_alarm}:{minutes_alarm}:{seconds_alarm}")
+    print()
+    while True:
+        hours = time.strftime('%I')
+        minutes = time.strftime('%M')
+        seconds = time.strftime('%S')
+        print(hours,':',minutes,':',seconds)
+        time.sleep(1)
+        if hours_alarm == hours and minutes_alarm == minutes and seconds_alarm == seconds:
+            print("ALARM !!!")
 
 
-# Infinite loop asking the user what to do
-while True:
-    menu = int(input("What do you want to do ? : Insert '1' to show local time | Insert '2' to edit time | Insert '3' to set an alarm | : "))
 
-    if menu == 1:  # Show local time
-        localtime()
+# Function Infinite loop asking the user what to do
+def main():
+    while True:
+        menu = int(input("What do you want to do ? : Insert '1' to show local time | Insert '2' to edit time | Insert '3' to set an alarm | : "))
 
-    if menu == 3:  # Set an alarm
-        alarm()
+        if menu == 1:  # Show local time
+            localtime()
+
+        if menu == 3:  # Set an alarm
+            alarm()
+# Version 12h
+def main_12h():
+    while True:
+        menu2 = int(input("What do you want to do ? : Insert '1' to show local time | Insert '2' to edit time | Insert '3' to set an alarm | : "))
+
+        if menu2 == 1:  # Show local time
+            localtime2()
+
+        if menu2 == 3:  # Set an alarm
+            alarm2()
+
+
+
+
+
+format = input("Do you want to use 24h (A) or 12h (B) format? : ")
+
+if format == 'A':
+    print("You use the 24h format")
+    main()
+
+if format == 'B':
+    print("You use the 12h format")
+    main_12h()
