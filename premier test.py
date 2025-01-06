541b8d4842efe1936d001c9c67b423a2ace855a6
Hippo <hippolyte.geslain@laplateforme.io>
2025-01-06 10:33:33 +0100

    Horloge avec data sous variables différentes

---
+++ premier test.py
@@ -0,0 +1,8 @@
+import time
+
+while True:
+    hours = time.strftime('%H')
+    minutes = time.strftime('%M')
+    seconds = time.strftime('%S')
+    print(hours,':',minutes,':',seconds)
+    time.sleep(1)
