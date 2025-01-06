8c484aa5eeb2253b1ae83317005a8969d2f7dca3
kylliann-larcher <kylliann.larcher@laplateforme.io>
2025-01-06 09:31:31 -0400

    Update horloge de mamie regler.py
    réglage alarme en ligne

--- horloge de mamie regler.py
+++ horloge de mamie regler.py
@@ -1,9 +1,14 @@
+""""Vous ajoutez une fonction nommée “afficher_heure” qui permet de
+régler l'heure. Cette fonction devra prendre en paramètre une heure
+sous la forme d'un tuple (heures, minutes, secondes) et devra mettre à
+jour l'heure affichée grâce à la fonction “afficher_heure”."""
+
 import time
-heures, minutes, secondes = 0, 0, 0
+heures, minutes, secondes = 0, 0, 0 #INITIALISATION DES VARIABLES
 
 def regler_heure(heure):
     global heures, minutes, secondes
-    heures, minutes, secondes = heure
+    heures, minutes, secondes = heure #INITIALISATION TUPLE HEURE
 
 def afficher_heure():
     global heures, minutes, secondes
@@ -18,9 +23,6 @@ def afficher_heure():
 
     if heures >= 24:
         heures = 0
-    heure_formattee = f"{str(heures).zfill(2)}:{str(minutes).zfill(2)}:{str(secondes).zfill(2)}"
-
-    print(heure_formattee)
 
 regler_heure((3, 30, 45))
 
